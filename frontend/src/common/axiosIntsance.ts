aff655a1155dc17de7c3e62aa83a0551d4121aec
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-03-31 19:20:40 +0300

    Creates docker-cpmpose

--- frontend/src/common/axiosIntsance.ts
+++ frontend/src/common/axiosIntsance.ts
@@ -10,7 +10,7 @@ import axios from "axios"
 
 
 const axiosInstance = axios.create  ({
-    baseURL: "http://localhost:4000/api/v1/"
+    baseURL: "http://localhost:5131/api/v1"
 })
 
 export default axiosInstance
